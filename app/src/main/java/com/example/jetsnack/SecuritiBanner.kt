a71f3dc0d1779a669bf94a50239434fb0eea9fe2
Rama <158269485+ramanarayana-narmala-securiti-ai@users.noreply.github.com>
2024-09-19 15:19:47 +0530

    Add SDK version log and a custom permission to the App

--- app/src/main/java/com/example/jetsnack/SecuritiBanner.kt
+++ app/src/main/java/com/example/jetsnack/SecuritiBanner.kt
@@ -8,6 +8,7 @@ import com.example.jetsnack.ui.MainActivity
 object SecuritiBanner {
 
     fun initializeBanner(activity: MainActivity) {
+        println("SECURITI_SDK_VERSION: ${SecuritiMobileCmp.VERSION}")
         println("Initializing Securiti Banner")
 
         val options = CmpSDKOptions(
@@ -15,7 +16,7 @@ object SecuritiBanner {
             cdnURL = "https://cdn-qa.securiti.xyz/consent",
             tenantID = "b9153366-8d40-43b0-8009-e7c5f7342b6c",
             appID = "e6c35be9-0159-4f5e-ad95-d7476ce39943",
-            testingMode = true,
+            testingMode = false,
             loggerLevel = CmpSDKLoggerLevel.DEBUG,
             backgroundTaskIntervalTime = 1
         )
